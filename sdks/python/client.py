eb128fea345ea3441d097f4d78ba944812d2b0df
BChip <bachippi@svsu.edu>
2017-10-21 13:47:56 -0400

    discovery

--- sdks/python/client.py
+++ sdks/python/client.py
@@ -30,20 +30,44 @@ class Game:
         self.units = {} # set of unique unit ids
         self.tiles = {}
         self.resourceCords = set()
-        self.directions = ['N', 'S', 'E', 'W']
+        self.first = True
+        self.discoveryUnits = []
+        self.command = {"commands": []}
 
     def gameResponse(self, json_data):
         self.updateUnits(json_data)
         self.updateTiles(json_data)
-
-        print json_data['tile_updates']
-        
-        unit = random.choice(tuple(self.units.keys()))
-        direction = 'S'
-        move = 'MOVE'
-        command = {"commands": [{"command": move, "unit": unit, "dir": direction}]}
-        response = json.dumps(command, separators=(',',':')) + '\n'
+        print self.resourceCords
+        if self.first:
+            response = self.firstMove()
+        self.checkDiscoveryIsIdle()
+        response = json.dumps(self.command, separators=(',',':')) + '\n'
         return response
+
+    def firstMove(self):
+        units = tuple(self.units.keys())
+        self.discoveryUnits = [units[1], units[2], units[3], units[4]]
+        self.units[units[1]].dir = "N"
+        self.units[units[2]].dir = "E"
+        self.units[units[3]].dir = "S"
+        self.units[units[4]].dir = "W"
+        self.command['commands'].append({"command": "MOVE", "unit": self.discoveryUnits[0], "dir": 'N'})
+        self.command['commands'].append({"command": "MOVE", "unit": self.discoveryUnits[1], "dir": 'E'})
+        self.command['commands'].append({"command": "MOVE", "unit": self.discoveryUnits[2], "dir": 'S'})
+        self.command['commands'].append({"command": "MOVE", "unit": self.discoveryUnits[3], "dir": 'W'})
+
+    def checkDiscoveryIsIdle(self):
+        for unit in self.discoveryUnits:
+            if self.units[unit].status == "idle":
+                if self.units[unit].dir == 'N':
+                    self.command['commands'].append({"command": "MOVE", "unit":unit, "dir": "E"})
+                elif self.units[unit].dir == 'E':
+                    self.command['commands'].append({"command": "MOVE", "unit":unit, "dir": "S"})
+                elif self.units[unit].dir == 'S':
+                    self.command['commands'].append({"command": "MOVE", "unit":unit, "dir": "W"})
+                else:
+                    self.command['commands'].append({"command": "MOVE", "unit":unit, "dir": "N"})
+
 
     def updateUnits(self, json_data):
         for unit in json_data['unit_updates']:
@@ -69,8 +93,14 @@ class Game:
             visible = tile['visible']
             x = tile['x']
             y = tile['y']
-            blocked = tile['blocked']
-            resources = tile['resources']
+            try:
+                blocked = tile['blocked']
+            except:
+                blocked = True
+            try:
+                resources = tile['resources']
+            except:
+                resources = None
             if resources:
                 self.resourceCords.add((x,y))
             else:
@@ -90,6 +120,7 @@ class Unit:
         self.health = health
         self.resources = resources
         self.can_attack = can_attack
+        self.dir = ""
 
 class Tile:
     def __init__(self,visible,x,y,blocked,resources):
